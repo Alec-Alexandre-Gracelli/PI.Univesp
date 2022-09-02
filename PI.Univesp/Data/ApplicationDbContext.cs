bbd8cd081ad908bc0bcac7093a96d1ad3e55abb8
Alec_Gracelli <alecgracelli@gmail.com>
2022-09-02 11:49:46 -0300

    Add project files.

---
+++ PI.Univesp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pi.Univesp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pi.Univesp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Pi.Univesp.Models.Nota> Nota { get; set; }
+
+    }
+}
---
+++ PI.Univesp/Models/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Pi.Univesp.Models
+{
+    public class Aluno
+    {
+        
+        //[Key]
+        public int AlunoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public int RA { get; set; }
+
+        public virtual ICollection<Nota> Notas { get; set; }
+
+
+    }
+}
